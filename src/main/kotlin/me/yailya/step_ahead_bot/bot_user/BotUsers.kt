f0d54b522b9c3ece9bc5b8d0dac27f3014b0b0af
ya-ilya <ilya.tseplaev@gmail.com>
2024-11-29 20:07:20 +0300

    Fixed creating BotUser

--- src/main/kotlin/me/yailya/step_ahead_bot/bot_user/BotUsers.kt
+++ src/main/kotlin/me/yailya/step_ahead_bot/bot_user/BotUsers.kt
@@ -5,8 +5,8 @@ import org.jetbrains.exposed.sql.javatime.datetime
 
 object BotUsers : IntIdTable() {
     val userId = long("userId")
-    val isModerator = bool("isModerator")
-    val isAdministrator = bool("isAdministrator")
+    val isModerator = bool("isModerator").default(false)
+    val isAdministrator = bool("isAdministrator").default(false)
     val lastQuestionTime = datetime("lastQuestionTime").nullable()
     val lastQuestionAnswerTime = datetime("lastQuestionAnswerTime").nullable()
     val lastTeacherReviewTime = datetime("lastTeacherReviewTime").nullable()
